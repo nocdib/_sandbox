684ba0517e4da4c68b7f13764812f54cf01e500e
nocdib <nocdib@gmail.com>
2015-01-19 23:30:48 -0500

    adding Django stuff

---
+++ pyFile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+
+print 'FIRST - ' + (os.path.dirname('~/_sandbox/python_stuff'))
+
+print os.path.abspath(os.path.dirname(__file__))
+
+print (os.path.dirname(__file__))
+
