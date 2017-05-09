30420f6b7c548c8a3d031b8e1da16d9d3789f230
Greg Joshua <nocdib@gmail.com>
2017-05-09 07:58:09 -0400

    oracle press java

---
+++ java_stuff/BeginnersGuide/src/com/nocdib/chapter02/CharacterIncrement.java
@@ -0,0 +1,11 @@
+package com.nocdib.chapter02;
+
+public class CharacterIncrement {
+
+    public static void main(String[] args) {
+    for(char x = 'A'; x < '夸'; x++){
+	        System.out.printf("%d) %c\n", (int)x , x);
+        }
+    }
+
+}
