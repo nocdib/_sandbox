8c9094c74ede57df40d59ef26702fa12964aef73
GregNYT <gregory.joshua@nytimes.com>
2017-07-16 23:30:13 -0400

    Finished module 5

---
+++ js_stuff/module05/arrayDestructuring.js
@@ -0,0 +1,10 @@
+const siblings = ['greg', 'gary', 'gizelle'];
+[first, second, third] = siblings;
+console.log(first, second, third);
+
+// Variable swapping
+let a = "one";
+let b = "two";
+console.log(a,b);
+[a,b]=[b,a];
+console.log(a,b);
