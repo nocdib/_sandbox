913da70412e1d035aac8b75ae46cf31b6657ac75
Greg Joshua <nocdib@gmail.com>
2017-11-23 13:49:51 -0500

    added files to from module 2

--- js_stuff/module02/arguments.js
+++ js_stuff/module02/arguments.js
@@ -5,5 +5,5 @@ const getArgs = function(){
    arr = args.map((val, idx) => ({val,idx}))
    console.log(arr);
 };
-
+getArgs();
 getArgs(1,2,3);
--- js_stuff/module02/arrowExample.js
+++ js_stuff/module02/arrowExample.js
@@ -5,5 +5,5 @@ let obj = names.map((name, rank) => ({name: name, rank: ++rank, species}));
 console.log(obj);
 
 let percentages = [10,20,30,40,50,60,70,80,90,100];
-let greaterThanHalf = percentages.filter((percentage) => (percentage > 50));
+let greaterThanHalf = percentages.filter((percentage) => (percentage > 50)).join('\n');
 console.log(greaterThanHalf);
