e2b5aca18a579361d54e0c77d68a7680472f3fbf
GregNYT <gregory.joshua@nytimes.com>
2017-12-18 10:54:57 -0500

    Finished Traversy 19/119

--- js_stuff/traversy_js_beginning/02_js_fundamentals/2_1_project_files/js_sandbox/app.js
+++ js_stuff/traversy_js_beginning/02_js_fundamentals/2_1_project_files/js_sandbox/app.js
@@ -1,1 +1,16 @@
-alert('Hello World');
+// alert('Hello World');
+
+const val = 100;
+let temp;
+
+if(val == '100'){
+  console.log('Passes == test');
+}
+if(val === '100'){
+  console.log('Passes === test');
+}
+
+const cars = ['nissan', 'honda', 'chevy'];
+
+cars.forEach(car => console.log(car));
+window.history.go(-1);
