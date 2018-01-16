066c1f73910b4025a8ff3301e727f0597cd7a977
Greg Joshua <nocdib@gmail.com>
2018-01-16 00:21:57 -0500

    restart Kotlin #23

---
+++ kotlin_stuff/src/collections/ArraysLists.kt
@@ -0,0 +1,19 @@
+package collections
+
+/**
+ * Created by cyclops on 1/15/18.
+ */
+fun main(args: Array<String>) {
+    val arrayVar = arrayOf("Beemer", "Benz", "Bentley")
+    val listVar = listOf("Nissan", "Honda", "Chevy")
+
+    println(arrayVar[0])
+    arrayVar[0] = "BMW"
+    println(arrayVar[0])
+
+    println(listVar[0])
+    //listVar. = "Toyota"
+    println(listVar[0])
+
+}
+
