1f3f1381fca733ae1023d94d9e7aad76ec5578d9
GregNYT <gregory.joshua@nytimes.com>
2018-12-27 11:02:02 -0500

    removed vim swap files and added code comments

--- js_stuff/free_codecamp_node_tutorial/import-export/services.js
+++ js_stuff/free_codecamp_node_tutorial/import-export/services.js
@@ -1,3 +1,13 @@
+/* Encapsulating the functions and variables in a single object allows it to 
+be exported into a variable and called in reference to that variable.
+
+e.g.
+
+const services = require('./services'); // import this file
+services.printOne(); // prints "One"
+services.printTwo(); // prints "Two"
+*/
+
 module.exports = {
 	printOne() {
 		console.log('One');
