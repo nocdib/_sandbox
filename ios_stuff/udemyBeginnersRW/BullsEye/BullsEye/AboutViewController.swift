cdec08cfdaeab3c88691dda030efa2446fcd9065
GregNYT <gregory.joshua@nytimes.com>
2019-04-02 16:37:10 -0400

    finished Bull's Eye app

---
+++ ios_stuff/udemyBeginnersRW/BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Joshua, Gregory on 3/22/19.
+//  Copyright © 2019 NOCDIB, Inc. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the HTML file into the Web View object
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    // Dismiss the About screen
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
--- ios_stuff/udemyBeginnersRW/BullsEye/BullsEye/ViewController.swift
+++ ios_stuff/udemyBeginnersRW/BullsEye/BullsEye/ViewController.swift
@@ -22,6 +22,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Start a new game
         startNewRound(newGame: true)
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: UIControl.State.normal)
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
     
     // Start a new round when false. Start a new game when true
@@ -51,25 +64,30 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         var alertTitle: String
-        var presentCompletion: () -> Void
+        var handlerAction: (Any) -> Void
         var buttonTitle: String
         let difference = abs(sliderValue - targetValue)
         scoreValue += difference
         // If the score is less than or equal to 10 then show a Next Round alert
         if (scoreValue <= 10) {
             alertTitle = "On To The Next Round"
-            presentCompletion = { self.startNewRound() }
+            handlerAction = { action in self.startNewRound() }
             buttonTitle = "OK"
         // If the score is greater than 10 show a Game Over alert
         } else {
             alertTitle = "You Completed \(roundValue-1) Round"
             alertTitle += roundValue-1 == 1 ? "" : "s"
-            presentCompletion = { self.startNewRound(newGame: true) }
+            handlerAction = { action in self.startNewRound(newGame: true) }
             buttonTitle = "New Game"
         }
         let alert = UIAlertController(title: alertTitle, message: "Slider: \(sliderValue)\nTarget: \(targetValue)\nScore: \(scoreValue)", preferredStyle: .alert)
-        let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        // Start a new round or game after the alert button is clicked.
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: handlerAction)
         alert.addAction(action)
-        present(alert, animated: true, completion: presentCompletion)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func startNewGame() {
+        self.startNewRound(newGame: true)
     }
 }
