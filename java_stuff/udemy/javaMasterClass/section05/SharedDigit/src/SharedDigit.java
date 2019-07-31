41165e32897b9e735276081545b0545d2dc6f54d
GregNYT <gregory.joshua@nytimes.com>
2019-07-30 20:20:13 -0400

    finished section 5 exercise 24

--- java_stuff/udemy/javaMasterClass/section05/SharedDigit/src/SharedDigit.java
+++ java_stuff/udemy/javaMasterClass/section05/SharedDigit/src/SharedDigit.java
@@ -3,20 +3,17 @@ public class SharedDigit {
     public static boolean hasSharedDigit(int num1, int num2) {
         if((num1 >= 10 && num1 <= 99) && (num2 >= 10 && num2 <= 99)) {
             int digit1, digit2, temp;
-            System.out.printf("num1: %d\nnum2: %d\n\n", num1, num2);
             while(num1 != 0) {
                 temp = num2;
                 digit1 = num1 % 10;
                 while(temp != 0) {
                     digit2 = temp % 10;
-                    System.out.printf("digit 1: %d\ndigit 2: %d\n\n", digit1, digit2);
                     if(digit1 == digit2) {
                         return true;
                     }
                     temp = temp/10;
                 }
                 num1 = num1/10;
-                System.out.printf("num1: %d\nnum2: %d\n\n", num1, num2);
             }
         }
         return false;
@@ -24,6 +21,8 @@ public class SharedDigit {
 
     public static void main(String args[]) {
         System.out.println(hasSharedDigit(11, 44));
+        System.out.println(hasSharedDigit(11, 31));
+        System.out.println(hasSharedDigit(11, 15));
 
     }
 }
