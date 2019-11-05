b0b78be408f74d7146a2223d4766aabcdc075236
nocdib <nocdib@valhalla.local>
2019-11-05 22:19:56 +0100

    finished section 8

---
+++ java_stuff/udemy/javaMasterClass/section11/UseSeries/src/com/nocdib/Main.java
@@ -0,0 +1,15 @@
+package com.nocdib;
+
+import com.nocdib.sumfactfib.Series;
+
+// Make use of the JAR created from the Series project.
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(Series.factorial(n));
+        System.out.println(Series.factorial(n));
+        System.out.println(Series.nSum(n));
+    }
+}
